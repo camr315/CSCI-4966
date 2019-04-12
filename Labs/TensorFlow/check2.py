409f717b562586d05cc7c5bfab48cb894ff3e08d
camr315 <camr315@gmail.com>
2019-04-12 14:31:20 -0400

    pictures

---
+++ Labs/TensorFlow/check2.py
@@ -0,0 +1,113 @@
+from __future__ import absolute_import, division, print_function
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+from PIL import Image
+from PIL import ImageOps
+
+img1 = Image.open("shirt1.jpg")
+img1 = img1.convert('L')
+img1 = ImageOps.invert(img1)
+img1 = img1.resize([28,28])
+img1.save("shirt1-2","JPEG")
+img2 = Image.open("shirt2.jpg")
+img2 = img2.convert('L')
+img2 = ImageOps.invert(img2)
+img2 = img2.resize([28,28])
+img2.save("shirt2-2","JPEG")
+img3 = Image.open("shirt3.jpg")
+img3 = img3.convert('L')
+img3 = ImageOps.invert(img3)
+img3 = img3.resize([28,28])
+img3.save("shirt3-2","JPEG")
+
+img1 = np.array(img1) / 255
+img2 = np.array(img2) / 255
+img3 = np.array(img3) / 255
+
+imgs = [img1, img2, img3]
+imgs = np.array(imgs)
+
+print(tf.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+'''plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+plt.show()'''
+
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+model.compile(optimizer='adam', 
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit(train_images, train_labels, epochs=5)
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print('Test accuracy:', test_acc)
+
+predictions = model.predict(imgs)
+
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    
+    plt.imshow(img, cmap=plt.cm.binary)
+  
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+    
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                  100*np.max(predictions_array),
+                                  class_names[true_label]),
+                                  color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array[i], true_label[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1]) 
+    predicted_label = np.argmax(predictions_array)
+ 
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+# Plot the first X test images, their predicted label, and the true label
+# Color correct predictions in blue, incorrect predictions in red
+
+num_rows = 1
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+    plt.subplot(num_rows, 2*num_cols, 2*i+1)
+    plot_image(i, predictions, test_labels, imgs)
+    plt.subplot(num_rows, 2*num_cols, 2*i+2)
+    plot_value_array(i, predictions, test_labels)
+plt.show()
